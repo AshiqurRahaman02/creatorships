35326769c18ec75fcbd91d27a72b300e7c263d7e
AshiqurRahaman02 <ashiqur999999@gamil.com>
2024-07-19 01:05:08 +0530

    fixed stuff

---
+++ src/pages/application/[id].tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function application() {
+  return (
+    <div>
+      application
+    </div>
+  )
+}
+
+export default application
---
+++ src/pages/application/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function index() {
+  return (
+    <div>
+      applications
+    </div>
+  )
+}
+
+export default index
